b16fee821592e56396e453961d3e60b6e670e64e
Vatsal <vatsalgada2006@gmail.com>
2026-08-21 04:03:54 +0530

    Add start_app.py

---
+++ start_app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(
+        "backend.backend.main:app",
+        host=host,
+        port=port,
+        reload=True
+    )
